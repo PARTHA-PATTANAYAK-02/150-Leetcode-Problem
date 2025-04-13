c8b63c34a8ea9cf28851d0b4f6d866238649c11b
Partha Pattanayak <parthapattanayak2002@gmail.com>
2025-04-13 11:14:07 +0530

    LeetCode-125

---
+++ TwoPointers/ValidPalindrome.java
@@ -0,0 +1,19 @@
+package TwoPointers;
+
+public class ValidPalindrome {
+    public static boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(str));
+    }
+
+}
