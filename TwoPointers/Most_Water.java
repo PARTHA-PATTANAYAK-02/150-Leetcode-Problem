5fe8971fe634d4ec04c61338aca99d8915342d3b
Partha Pattanayak <parthapattanayak2002@gmail.com>
2025-04-15 21:58:01 +0530

    LeetCode-11

---
+++ TwoPointers/Most_Water.java
@@ -0,0 +1,21 @@
+package TwoPointers;
+
+public class Most_Water {
+    private static int maxArea(int height[]) {
+        int maxWater = 0;
+        int left = 0, right = height.length - 1;
+        while (left < right) {
+            maxWater = Math.max(maxWater, (right - left) * Math.min(height[left], height[right]));
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxWater;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxArea(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }));
+    }
+}
